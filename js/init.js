f88769b1dff7d9546cf2a40e35a23be117478707
bazhinofficial <alexbazhyn@gmail.com>
2018-07-20 02:43:15 +0300

    Some changes in structure and revision navbar

--- js/init.js
+++ js/init.js
@@ -11,5 +11,12 @@
         $('body,html').animate({scrollTop: top}, 1500);
     });
 
+    $(window).scroll(function() {
+        if ($(window).scrollTop() >= $(window).height()) {
+            $('nav').addClass('white').addClass('fixed').removeClass('inherit');
+        } else {
+            $('nav').addClass('inherit').removeClass('white').removeClass('fixed');
+        }
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
