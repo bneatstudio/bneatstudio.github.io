b7c64b214bee7c575a63ba9d9ddf413d99833d23
bazhinofficial <alexbazhyn@gmail.com>
2018-07-19 03:24:03 +0300

    Add some content and functionality

--- js/init.js
+++ js/init.js
@@ -4,5 +4,12 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
 
+    $("#menu").on("click", "a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
