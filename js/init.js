d58cec65c8799ee14671b92c887517efede2634a
bazhinofficial <alexbazhyn@gmail.com>
2018-07-22 03:55:21 +0300

    Some changes #IOS1

--- js/init.js
+++ js/init.js
@@ -5,18 +5,19 @@
     $('.parallax').parallax();
 
     $("#menu").on("click", "a", function (event) {
+        var heightHeader = 60;
         event.preventDefault();
         var id  = $(this).attr('href'),
-            top = $(id).offset().top;
+            top = $(id).offset().top - heightHeader;
         $('body,html').animate({scrollTop: top}, 1500);
     });
 
     $(window).scroll(function() {
-        if ($(window).scrollTop() >= $(window).height()) {
+        if ($(window).scrollTop() >= $(window).height() - 64) {
             $('nav').addClass('white').addClass('fixed').removeClass('inherit');
         } else {
             $('nav').addClass('inherit').removeClass('white').removeClass('fixed');
         }
     });
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+  });
+})(jQuery);
