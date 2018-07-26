f66ac9641ce7ea9e28713f1a62cbed6583f28cd0
bazhinofficial <alexbazhyn@gmail.com>
2018-07-26 03:21:12 +0300

    Changes #IOS1

--- js/init.js
+++ js/init.js
@@ -4,7 +4,7 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
 
-    $("#menu").on("click", "a", function (event) {
+    $(".anchor-link").on("click", "a", function (event) {
         var heightHeader = 60;
         event.preventDefault();
         var id  = $(this).attr('href'),
@@ -18,6 +18,28 @@
         } else {
             $('nav').addClass('inherit').removeClass('white').removeClass('fixed');
         }
+    });
+
+    $("#form-contact").submit(function(e) {
+        e.preventDefault();
+        var formData = $(this).serialize();
+        $.ajax({
+            url: "https://docs.google.com/forms/d/e/1FAIpQLSdFyjokPUuQnsucvT-7LemVyXUsDDYG-rLAHVnalyGJXhVWVg/formResponse", // слать надо сюда, строку с буковками надо заменить на вашу, это атрибут action формы
+            data: formData,
+            type: "POST",
+            dataType: "xml",
+            beforeSend: function() {
+                $(this).find('button').attr('disabled');
+            },
+            statusCode: {
+                0: function () {
+                    $(this).html('<h4>Спасибо!</h4><p>Форма отправлена блаблабла</p>');
+                },
+                200: function () {
+                    $(this).html('<h4>Спасибо!</h4><p>Форма отправлена блаблабла</p>');
+                }
+            }
+        });
     });
   });
 })(jQuery);
